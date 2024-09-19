b042222b6ed4eec97bc02ce7a7d992edbf3c0086
Timur Ostapenko <ostapenko_timurka00@mail.ru>
2024-09-19 17:54:19 +0700

    Создан .gitignore

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на Flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Остапенко Тимур Ростиславович, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Остапенко Тимур, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
