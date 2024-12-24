f6fe1f69e5bde3b16ae745e23005889edfd21c00
Timur Ostapenko <ostapenko_timurka00@mail.ru>
2024-12-24 19:00:37 +0700

    Модель бд

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -7,12 +11,29 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 from RGZ import RGZ
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'совершенно секретно')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+        db_name = "WEB" 
+        db_user = 'postgres' 
+        db_password = 'Super0925!'
+        host_ip = '127.0.0.1'
+        host_port = 5432
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+
+else:
+    dir_path = path.dirname(path.realpath(__file__)) 
+    db_path = path.join(dir_path, "timur.db") 
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -21,6 +42,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 app.register_blueprint(RGZ)
 
 
